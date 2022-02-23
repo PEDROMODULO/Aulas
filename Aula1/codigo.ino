5c8f05d62cce2872a86d8aca34eaf679b5081d33
PEDROMODULO <100300611+PEDROMODULO@users.noreply.github.com>
2022-02-23 12:51:41 -0300

    Create codigo.ino

---
+++ Aula1/codigo.ino
@@ -0,0 +1,19 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}pinmode(4,OUTPUT)
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200); )  
+    
+digitalWrite(3, HIGH);
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200); )
+}
